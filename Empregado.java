63123af3ea3c7cd7de7e7e672e96192d3c33e00a
Loredana Michelle <loredanacordero.5591@aluno.saojudas.br>
2021-03-17 22:34:54 -0300

    primeiro teste da folha de pagamento executou com sucesso

--- Empregado.java
+++ Empregado.java
@@ -1,45 +1,61 @@
 public class Empregado{
+    private String nome;
+    private int idade;
     private int tipo;
     private double salario;
     private double comissao;
     private double bonus;
-    private String nome;
-    private int idade;
-
-    public int getTipo(){
-        return tipo;
+  
+    public double calculaSalario (){
+      switch (tipo){
+        case 1:
+          return salario;
+        case 2:
+          return salario + salario * comissao;
+        case 3:
+          return salario + bonus;
+        default:
+          return 0;
+      }
     }
+  
     public void setTipo(int tipo) {
-        this. tipo = tipo;
+      this.tipo = tipo;
     }
-
-    public double calcularSalario(){
-        switch (tipo) {
-            case 1:
-                return salario;
-            case 2:
-                return salario + salario * comissao;
-            case 3:
-                return salario + bonus;
-            default:
-                return 0;
-        }
+  
+    public int getTipo() {
+      return tipo;
     }
-
-    public int getIdade() {
-        return idade;
-    }
-
+  
     public String getNome() {
-        return nome;
+      return nome;
     }
-
-    public void setIdade(int idade){
-        this.idade = idade;
+    public int getIdade() {
+      return idade;
     }
-
+    public void setIdade(int idade) {
+      this.idade = idade;
+    }
     public void setNome(String nome) {
-        this.nome = nome;
+      this.nome = nome;
     }
-
-}
+    public double getBonus() {
+      return bonus;
+    }
+    public double getComissao() {
+      return comissao;
+    }
+    public double getSalario() {
+      return salario;
+    }
+    public void setBonus(double bonus) {
+      this.bonus = bonus;
+    }
+    public void setComissao(double comissao) {
+      this.comissao = comissao;
+    }
+    public void setSalario(double salario) {
+      this.salario = salario;
+    }
+  }
+  
